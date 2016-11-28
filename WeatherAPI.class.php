4fc2bfdb8e3e0ac412e11403bb73d8aeb6650ddf
zdc <dragonzhu1988@126.com>
2016-11-28 11:11:19 +0800

    Rejected commit: Default

--- WeatherAPI.class.php
+++ WeatherAPI.class.php
@@ -9,21 +9,6 @@
 
 header("Content-type:text/html;charset=utf-8");
 
-//$ch = curl_init();
-//$url = 'http://apis.baidu.com/apistore/weatherservice/citylist?cityname=%E6%9C%9D%E9%98%B3';
-//$header = array(
-//    'apikey: 01a24c0bb4aef2f6ec728501231ac4f5',
-//);
-//// 添加apikey到header
-//curl_setopt($ch, CURLOPT_HTTPHEADER  , $header);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//// 执行HTTP请求
-//curl_setopt($ch , CURLOPT_URL , $url);
-//$res = curl_exec($ch);
-//
-//var_dump(json_decode($res));
-
-
 class Weather
 {
     private $_weatherUrl = 'http://apis.baidu.com/apistore/weatherservice/citylist';
